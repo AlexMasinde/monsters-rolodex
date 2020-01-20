f857d2e3124b63a80160a967deffb1d01544fb7e
kingmasinde <daktari2033@gmail.com>
2020-01-20 13:01:24 +0300

    Updates

---
+++ precache-manifest.047ce8832fcc3dd0ec320cec33fd508f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03dde1278fd84f5323bef8f0c1e3ece7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cc264f0d5363a9c538e7",
+    "url": "/monsters-rolodex/static/css/main.7ec8eab5.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc264f0d5363a9c538e7",
+    "url": "/monsters-rolodex/static/js/main.239f4b72.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
